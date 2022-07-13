8b9908fbf443c8d03c7218005a73d3392d73e367
Mark Weaver <mark@weaver.life>
2022-07-13 15:04:12 -0700

    Add gitconfig files

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 ln -s ~/dotfiles/bash_aliases ~/.bash_aliases
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+mkdir -p ~/src/personal
+ln -s ~/dotfiles/personal_gitconfig ~/src/personal/.gitconfig
 
 sudo apt-get install -y vim jq
 
